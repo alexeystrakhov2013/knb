518b47e4b7f20a7234daa998670325078ceae847
strakhovdenya <strakhov.denya@gmail.com>
2026-03-13 17:00:33 +0100

    add button start and exit

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const scissors = "s";
 const paper = "p";
 const PcWin = "Pc is win";
 const exit = "exit";
+let exitState = "start"
 
 let countPlayerWin = 0;
 let countPcWin = 0;
@@ -10,23 +11,23 @@ let countPcWin = 0;
 const PlayerWin = "Player is win";
 let playerChois;
 
-function messageChois(chois){
-    if(chois == rock){
+function messageChois(chois) {
+    if (chois == rock) {
         return "rock";
     }
 
-    if(chois == scissors){
+    if (chois == scissors) {
         return "scissors";
     }
 
-    if(chois == paper){
+    if (chois == paper) {
         return "paper";
     }
 
     return "ooops";
 }
 
-function message(playerCh, pcch, win){
+function message(playerCh, pcch, win) {
     let playerChMessage = messageChois(playerCh);
     let pcChoiseMessage = messageChois(pcch)
     let message = `Player has ${playerChMessage} and Pc has ${pcChoiseMessage} ${win}!!!!!`;
@@ -34,54 +35,65 @@ function message(playerCh, pcch, win){
 }
 
 
-while (playerChois != exit) {
-    playerChois = prompt(`player win = ${countPlayerWin}, PC win ${countPcWin} 
+function start() {
+    exitState = "start"
+    while (exitState != exit) {
+        playerChois = prompt(`player win = ${countPlayerWin}, PC win ${countPcWin} 
         введите rock(r), scissors(s), paper(p) or (exit)`);
 
-    let Pchois = [rock, scissors, paper][Math.random() * 3 | 0];
+        if (playerChois === null) {
+            alert("Игра завершена");
+            exitState = exit;
+            break;
+        }
 
-    // ROCk && SCISSORS
-    if (playerChois == rock && Pchois == scissors) {
-        countPlayerWin++;
-        alert(message(rock, scissors , PlayerWin));
-    }
+        let Pchois = [rock, scissors, paper][Math.random() * 3 | 0];
 
-    if (playerChois == scissors && Pchois == rock) {
-        countPcWin++;
-         alert(message(scissors, rock , PcWin));
-    }
 
-    // PAPER && ROCK
-    if (playerChois == paper && Pchois == rock) {
-        countPlayerWin++;
-        alert(message(paper, rock , PlayerWin));
-    }
 
-    if (playerChois == rock && Pchois == paper) {
-        countPcWin++;
-        alert(message(rock, paper , PcWin));
-    }
+        // ROCk && SCISSORS
+        if (playerChois == rock && Pchois == scissors) {
+            countPlayerWin++;
+            alert(message(rock, scissors, PlayerWin));
+        }
 
-    // PAPER && SCISSORS
-    if (playerChois == scissors && Pchois == paper) {
-        countPlayerWin++;
-        alert(message(scissors, paper , PlayerWin));
-    }
+        if (playerChois == scissors && Pchois == rock) {
+            countPcWin++;
+            alert(message(scissors, rock, PcWin));
+        }
 
-    if (playerChois == paper && Pchois == scissors) {
-        countPcWin++;
-        alert(message(paper, scissors , PcWin));
-    }
+        // PAPER && ROCK
+        if (playerChois == paper && Pchois == rock) {
+            countPlayerWin++;
+            alert(message(paper, rock, PlayerWin));
+        }
 
-    // draw
-    if (playerChois == Pchois) {
-        alert(`player and pc has ${playerChois} is draw`)
-    }
+        if (playerChois == rock && Pchois == paper) {
+            countPcWin++;
+            alert(message(rock, paper, PcWin));
+        }
 
-    //EXIT
-    if (playerChois == exit){
-        alert("bay, bay");
-        break;
+        // PAPER && SCISSORS
+        if (playerChois == scissors && Pchois == paper) {
+            countPlayerWin++;
+            alert(message(scissors, paper, PlayerWin));
+        }
+
+        if (playerChois == paper && Pchois == scissors) {
+            countPcWin++;
+            alert(message(paper, scissors, PcWin));
+        }
+
+        // draw
+        if (playerChois == Pchois) {
+            alert(`player and pc has ${playerChois} is draw`)
+        }
+
+        //EXIT
+        if (playerChois == exit) {
+            exitState = exit;
+            alert("bay, bay");
+            break;
+        }
     }
 }
-
